793c57b3de09b939ed0ddcb36130b83bc6cca078
OlenaSemankiv <89794361+OlenaSemankiv@users.noreply.github.com>
2021-11-10 00:30:54 +0200

    hw30js

---
+++ Home work 30/jsFunction.js
@@ -0,0 +1,61 @@
+// ---------task1-----------
+// function summ(a, b) {
+//     if (a < b) {
+//         return (-1);
+//     }
+//     else if (a > b) {
+//         return (1);
+//     }
+//     else if (a = b) {
+//         return (0);
+//     }
+// }
+// let result = summ(2, 2);
+// alert( result )
+
+// --------- task2----------
+//     function factorial(n) {
+//         return (n != 1) ? n * factorial(n - 1) : 1;
+//     }
+
+// alert(factorial(4));
+
+// --------- task3----------
+// let number = (a,b,c) => (`${a}${b}${c}`);
+// alert( number (1,2,3) );
+
+// --------- task4----------
+// function check(a, b = 0) {
+//     if (b == 0) { return a ** 2; }
+//     return a * b;
+// }
+
+// console.log(check(5));
+// console.log(check(2, 4));
+
+// --------- task5----------
+
+// --------- task6----------
+
+// --------- task7----------
+// function setTime(h) {
+//     return function setMinutes(m = '00') {
+//       return function setSeconds(s = '00') {
+//         return `${h}:${m}:${s}`
+//       }
+//     }
+//   }
+
+//   let time = setTime(12)()();
+//   console.log(time);
+
+// --------- task8----------
+// h = +prompt('hour');
+// m = +prompt('minute');
+// s = +prompt('seconds');
+// function seconds (a,b,c) {
+// return (h * 3600) + (m * 60) + s
+// }
+// alert(seconds(h,m,s))
+
+// --------- task9----------
